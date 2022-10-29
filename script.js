8e487488f1dc11b1f4c2ea2cc3946dcdddcea9b9
Danahq8 <alomrandanah@gmail.com>
2022-10-29 20:15:02 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+function bmi( weight,heigh){
+    let result= weight/(heigh*heigh);
+    return result;
+}
+console.log(bmi(80,1.8));
+function status (bodymass){
+    if(bodymass < 18.5){
+        return "لديك نقص في الوزن"
+    } else if(bodymass>=18.5, bodymass<25){
+        return "وزنك صحي"
+    }
+
+     else if (bodymass>=25){
+        return "لديك زيادة في الوزن"
+     }
+}
+
+function calculate(){
+   let weight=document.getElementById("weight").value
+
+
+let heigh=document.getElementById("height").value
+let bmi_value = bmi(weight,heigh);
+let desc = status (bmi_value);
+
+let box = document.getElementById('result');
+ box.innerText = bmi_value + " == " + desc
+}
+
 
